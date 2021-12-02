ec7189d1a51927cdfff9f21345e8fd2c7b9a157b
Alex Kramer <alex.k.kramer@pwc.com>
2021-12-02 09:16:19 -0500

    added GETs

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const { MongoClient, ObjectId } = require('mongodb');
 const dotenv = require('dotenv');
 dotenv.config();
-// import { ObjectID } from 'bson';
-// or as an es module:
-// import { MongoClient } from 'mongodb'
+const express = require('express');
+const app = express();
+const port = 3000;
 
 // Connection URL
 const url = process.env.DB_URL
@@ -12,18 +12,20 @@ const client = new MongoClient(url);
 // Database Name
 const dbName = process.env.DB_NAME
 
-async function apiCall(type,collect,data,id){
-  console.log(">>>",data);
+ async function apiCall(type,collect,data,id){
+  
   await client.connect();
   const db = client.db(dbName);
   const collection = db.collection(collect)
   if(type == 'get' && !id){
     const findResult = await collection.find({}).toArray();
-    console.log('Found documents =>', findResult);
+    return findResult;
+    //console.log('Found documents =>', findResult);
   }
   else if(type == 'get' && id) {
     const findResult = await collection.findOne({"_id":id});
-    console.log('Found documents =>', findResult);
+    return findResult;
+   // console.log('Found documents =>', findResult);
 
   }
   else if (type == 'post') {
@@ -37,7 +39,33 @@ async function apiCall(type,collect,data,id){
   }
 }
 
-apiCall('get','restaurants',[], new ObjectId("5eb3d668b31de5d588f4292f"))
-.then(console.log)
+app.get('/neighborhoods', function(req, res) {
+  apiCall('get','neighborhoods',[])
+  .then((data) => {
+    res.set('Content-Type', 'application/json');
+    res.set('charset','utf-8');
+    res.json(data);
+  })
   .catch(console.error)
   .finally(() => client.close());
+});
+
+app.get('/restaurants', function(req, res) {
+  apiCall('get','restaurants',[])
+  .then((data) => {
+    res.set('Content-Type', 'application/json');
+    res.set('charset','utf-8');
+    res.json(data);
+  })
+  .catch(console.error)
+  .finally(() => client.close());
+});
+
+app.listen(port, function() {
+  console.log(`Example app listening on port ${port}!`)
+});
+
+// apiCall('get','restaurants',[], new ObjectId("5eb3d668b31de5d588f4292f"))
+// .then(console.log)
+//   .catch(console.error)
+//   .finally(() => client.close());
